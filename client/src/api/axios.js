20f444f32a83d7d50cd9f5a58823c2af2e0bedb0
vishvas-123 <246301227@gkv.ac.in>
2026-09-18 17:07:47 +0530

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://my-3year-4.onrender.com',
   headers: {
     'Content-Type': 'application/json'
   }
